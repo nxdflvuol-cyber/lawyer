6594d9597c79982f8a33650d2294ca8b70111974
Z User <z@container>
2026-06-28 18:17:43 +0000

    d6f699cf-af87-4d80-b600-011d495d36d5

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: ["http://21.0.7.133:3000", "http://localhost:3000", "http://127.0.0.1:3000"],
 };
 
 export default nextConfig;
